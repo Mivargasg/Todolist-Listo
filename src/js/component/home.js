b7cff129ee84cd80d3ffb5c536d93204f5b37b73
Mivargasg <miguelvargasgonzalezcr@gmail.com>
2021-03-16 19:00:41 +0000

    Listo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [tareas, setTareas] = useState("");
+	const [lista, setLista] = useState([]);
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1> Todo List</h1>
+			<input
+				value={tareas}
+				onChange={e => {
+					setTareas(e.target.value);
+				}}
+				onKeyPress={e => {
+					if (e.key == "Enter") {
+						setLista(lista.concat(tareas));
+						setTareas("");
+						console.log("Mi lista", lista);
+					}
+				}}
+			/>
+			{lista.map((item, index) => {
+				return (
+					<li
+						key={index}
+						onDoubleClick={() => {
+							setLista(
+								lista.filter(
+									(itemf, indexf) => indexf !== index
+								)
+							);
+						}}>
+						{item}
+					</li>
+				);
+			})}
 		</div>
 	);
 }
